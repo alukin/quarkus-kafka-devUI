9f915f3cd6844695da85399c867317901c856a5c
Oleksiy Lukin <alukin@gmail.com>
2022-07-22 10:01:42 +0300

    Move everything usefull to  kafka0ui dir; fix of links and icons

--- kafka-quickstart-ui/producer/src/main/resources/META-INF/resources/config.js
+++
@@ -1,4 +0,0 @@
-export const api = "kafka-admin";
-export const logo = "../q/dev/resources/images/quarkus_icon_rgb_reverse.svg";
-export const faviconLogo = "../q/dev/resources/images/favicon.ico";
-export const ui = "";
