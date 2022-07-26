bdd70a7862fee69f4d8c22fc26cff8712b5f146d
Oleksiy Lukin <alukin@gmail.com>
2022-07-26 08:43:52 +0300

    proper quote chart in config.js

--- kafka-quickstart-ui/producer/src/main/resources/META-INF/resources/kafka-ui/config.js
+++ kafka-quickstart-ui/producer/src/main/resources/META-INF/resources/kafka-ui/config.js
@@ -1,4 +1,4 @@
-export const api = "/q/kafka-admin";
-export const logo = "quarkus_icon_rgb_reverse.svg";
-export const faviconLogo = "favicon.ico";
-export const ui = "";
+export const api = '/q/kafka-admin';
+export const logo = 'quarkus_icon_rgb_reverse.svg';
+export const faviconLogo = 'favicon.ico';
+export const ui = '';
